3d1c232f1a513c36ce710b174114f6e3ca85e681
JimmyHuynh <jimmybhuynh@hotmail.com>
2018-12-04 18:49:31 -0800

    Layout Design
    initial design concept of pedometer

--- LifestyleApp/screens/PedometerScreen.js
+++ LifestyleApp/screens/PedometerScreen.js
@@ -1,53 +1,64 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import {
     View,
     Text,
     StyleSheet,
     ScrollView,
-    ImageBackground
+    ImageBackground,
+    Image
 } from "react-native";
-import {Header, Left, Right, Body ,Icon, Title, Button} from 'native-base'
+import { Container, Header, Left, Right, Body, Icon, Title, Content, Thumbnail, Button } from 'native-base'
 
 class PedometerScreen extends Component {
-//Button Design
+    //Button Design
     static navigationOptions = {
-        drawerIcon: ({tintColor}) => (
-            <Icon name="pulse" style={{fontSize: 24, color: tintColor}} />
+        drawerIcon: ({ tintColor }) => (
+            <Icon name="pulse" style={{ fontSize: 24, color: tintColor }} />
         )
     }
-    render(){
-        return(
-            <View style ={styles.container}>
-            <ImageBackground source={require('../assets/images/background.jpg')} style={{width: '100%', height: '100%'}}>
-                {/*Header Tag Contains the navigation menu*/}
-                <Header>
-                    <Left style = {{flex:0}}>
-                        <Icon name = "menu" onPress={()=>this.props.navigation.openDrawer()}/>
-                    </Left>
-                <Body style ={styles.center}> 
-                <Title style ={styles.title}>Lifeaholic</Title>
-                </Body>
-                <Right style = {{flex:0}}>
-                <Icon name = "person" onPress={()=>this.props.navigation.openDrawer()}/>
-                </Right>  
-                </Header>
-                <ScrollView>
-                    <View style ={styles.center}>
-                   
-                    <Text style = {styles.header}>Pedometer</Text>
-                    
-                    </View>
-                    
-                </ScrollView>
-                </ImageBackground>
-            </View>
+    render() {
+        const uri = "https://cdn4.iconfinder.com/data/icons/positive-character-traits-alphabet-d/210/positive-d005-512.png";
+        return (
+            <Container>
+                <View style={styles.container}>
+                    <ImageBackground source={require('../assets/images/background.jpg')} style={{ width: '100%', height: '100%' }}>
+                        {/*Header Tag Contains the navigation menu*/}
+                        <Header>
+                            <Left style={{ flex: 0 }}>
+                                <Icon name="menu" onPress={() => this.props.navigation.openDrawer()} />
+                            </Left>
+                            <Body style={styles.center}>
+                                <Title style={styles.title}>Lifeaholic</Title>
+                            </Body>
+                            <Right style={{ flex: 0 }}>
+                                <Icon name="person" onPress={() => this.props.navigation.openDrawer()} />
+                            </Right>
+                        </Header>
+                        <Content>
+                            <ScrollView>
+                                <View style={styles.center}>
+                                    <Text style={styles.header}>Pedometer</Text>
+                                    <Image
+                                        style={{ width: 150, height: 100 }}
+                                        resizeMode="contain"
+                                        source={{ uri: uri }}
+                                    />
+                                    <Text style={styles.header}> 1000 </Text>
+                                    <Text style={styles.text}> steps </Text>
+                                    <Text style={styles.header}>  Goal: 2000 </Text>
+                                </View>
+                            </ScrollView>
+                        </Content>
+                    </ImageBackground>
+                </View>
+            </Container>
         );
     }
 }
 export default PedometerScreen;
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
     },
     center: {
@@ -57,17 +68,17 @@ const styles = StyleSheet.create({
     },
     title: {
         fontWeight: 'bold',
-        fontSize: 25 , 
+        fontSize: 25,
         color: 'white'
     },
     text: {
         fontWeight: 'bold',
-        fontSize: 25 , 
+        fontSize: 25,
         color: 'white'
     },
     header: {
         fontWeight: 'bold',
-        fontSize: 40 , 
+        fontSize: 40,
         color: 'white'
     }
 })
